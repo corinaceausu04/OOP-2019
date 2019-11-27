3309ce3d4d3c8d6329c3cf991bb98a5f723aa346
corinaceausu04 <57182337+corinaceausu04@users.noreply.github.com>
2019-11-27 19:49:30 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,150 +1,438 @@
 #include <iostream>
-
+#include <string>
+#include <fstream>
+#define NULL 0
 using namespace std;
+ifstream f("lista_cumparaturi");
+std::ofstream out;
 
-class vec
-{
-    int *v;
-    int n;
+class Produse{
+protected:
+    float pret;
+    float cost;
+    int stoc;
+    char raspuns;
 public:
-    vec();
-    vec(const vec &);
-    vec(int sz,int x);
-    ~vec();
-    vec& operator = (const vec &ob);
-    void reactualizare(int y,int dim);
-    void suma();
-    void maxim();
-    void sortare();
-    int operator * (vec const &ob);
-    friend istream & operator >> (istream &, vec &);
-    friend ostream & operator << (ostream &, vec &);
-    static vec * cma();
+    Produse();
+    Produse(const Produse &a);
+    ~Produse();
+    friend istream & operator >> (istream &, Produse &);
+    friend ostream & operator << (ostream &, Produse &);
+    Produse& operator = (const Produse &ob);
+    virtual int calcul_cost(){return cost;}
 };
 
-istream & operator >> (istream &in, vec &ob){       //supraincarcarea operatorului ">>"
-    cout<<"Introduceti numarul de elemente ";
-    in>>ob.n;
-    cout<<"Introduceti elementele ";
-    for(int i=0;i<ob.n;i++)
-    in>>ob.v[i];
+istream & operator >> (istream &in, Produse &ob){
+    in>>ob.pret;
+    in>>ob.cost;
+    in>>ob.stoc;
     return in;
 }
-ostream & operator << (ostream &out, vec &ob){      //supraincarcarea operatorului "<<"
-out<<"Vectorul are "<<ob.n<<" elemente."<<endl;
-out<<"Elementele sunt ";
-for(int i=0;i<ob.n;i++)
-    out<<ob.v[i]<<" ";
-    out<<endl;
-return out;
+ostream & operator << (ostream &out, Produse &ob){
+    out<<ob.pret;
+    out<<ob.cost;
+    out<<ob.stoc;}
+Produse& Produse::operator = (const Produse &ob){
+    this->cost = ob.cost;
+    this->pret = ob.pret;
+    this->stoc = ob.stoc;
 }
-vec::vec(){                                         //initializare ob cu 0
-    this->v=new int[this->n];
-    this->n=0;
-for(int i=0;i<this->n;i++)
-    this->v[i]=0;
+Produse::~Produse(){
+    this->pret = 0;
+    this-> cost = 0;
+    this ->stoc = 0;
+}
+Produse::Produse(const Produse &a){   //constructor de copiere
+    this->pret = a.pret;
+    this->cost = a.cost;
+    this->stoc = a.stoc;
+}
+Produse::Produse(){          //constructor de initializare cu 0
+    this->pret = 0;
+    this->cost = 0;
+    this->stoc = 0;
+}
+class Varza: public Produse{
+    int nr_bucati;
+public:
+    Varza();
+    Varza(cost Varza &v);
+    int calcul_cost();
+};
+class Faina: public Produse{
+    int nr_kg;
+    int calitate;
+public:
+    int calcul_cost();
+};
+class Bere: public Produse{
+    int nr_bucati;
+    string brand;
+public:
+    int calcul_cost();
+};
+class Cartofi: public Produse{
+    int nr_kg;
+    string tip;
+public:
+    int calcul_cost();
+
+};
+class Vin_varsat: public Produse{
+    int litri;
+    string tip;
+public:
+    int calcul_cost();
+};
+class vin_soi: public Produse{
+    int buc;
+    string soi;
+    int an_culegere;
+    string tara_origine;
+public:
+    int calcul_cost();
+};
+class Jucarii: public Produse{
+    int nr_bucati;
+    string nume;
+public:
+    int calcul_cost();
+};
+int Jucarii::calcul_cost(){
+    f>>nume;
+    f>>nr_bucati;
+    if(nume == "Lego")
+       {
+           pret = 50;
+           stoc = 10;}
+    else if(nume == "Masinuta")
+        {pret = 30;
+        stoc = 20;}
+    else if(nume == "Papusa Barbie")
+        {pret == 60;
+        stoc = 15;}
+    else if(nume == "Trenulet")
+        {pret == 100;
+        stoc = 30;}
+    else
+        cout<<"Produsul nu este in stoc."<<endl;
+    if(nr_bucati > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de bucati de"<<nume<<". Doriti sa cumparati "<< stoc << " de bucati?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<nr_bucati; i++)
+        cost = pret * nr_bucati;
+    cout<<cost;
+    return cost;
+
 
 }
-vec& vec::operator = (const vec &ob){               //supraincarcarea op "="
-    if(this!= &ob)
-    {
-        this->n = ob.n;
-        this->v = ob.v;
-
-    }
-    cout<<"Apel supraincarcare =\n";
-    return *this;
+int Cartofi::calcul_cost(){
+    f>>nr_kg;
+    f>>tip;
+    if(tip == "rosii")
+        {pret = 2;
+        stoc = 50;}
+    else if(tip == "albi")
+        {pret == 3;
+        stoc = 100;}
+    else if(tip.empty())
+        tip = "rosii";
+    if(nr_kg > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de kilograme de cartofi "<<tip<<". Doriti sa cumparati "<< stoc << " de kilograme?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<nr_kg; i++)
+        cost = nr_kg * pret;
+    cout<<cost<<endl;
+    return cost;
 }
-vec::vec(const vec &a){                             //constr de copiere
-
-    this->n = a.n;
-    this->v = new int[a.n];
-    for(int i=0; i<a.n; i++)
-        this->v[i]=a.v[i];
-
+int Bere::calcul_cost(){
+    f>>nr_bucati;
+    f>>brand;
+    if(brand == "peroni")
+        {pret = 5;
+        stoc = 100;}
+    else if(brand == "tuborg")
+        {pret = 3;
+        stoc = 200;}
+    else if(brand.empty())
+        brand = "tuborg";
+    else cout<<"Produsul nu este in stoc."<<endl;
+     if(nr_bucati > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de bucati de bere "<<brand<<". Doriti sa cumparati "<< stoc << " de bucati?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<nr_bucati; i++)
+            cost = pret * nr_bucati;
+    cout<<"Costul pentru bere este: "<<cost<<endl;
+    return cost;
 }
-vec::vec(int sz, int x){                            //init cu un nr pe toate componentele
-        this->n=sz;
-        this->v = new int[sz];
-        for(int i=0; i<sz; i++)
-            v[i]=x;
-
-    }
-vec::~vec(){                                        //destructor pt dezalocare zona de mem
-        for(int i=0; i<this->n; i++)
-            delete []v;}
-void vec::reactualizare(int y,int dim){             //reactualizarea nr de componente si initializarea cu y pe toate comp
-        cout<<"Am reactualizat\n";
-        this->v = new int[dim];
-        for(int i=0; i<dim; i++)
-            v[i]=y;
-        n = dim;
-    }
-void vec::suma(){                                   //suma elementelor din vector
-        int s=0;
-        for(int i=0; i<this->n; i++)
-            s=s+v[i];
-        cout<<"Suma elementelor vectorului este "<<s<<endl;
-    }
-void vec::maxim(){                                  //elementul maxim din vector
-        int max=0,poz;
-        for(int i=0;i<this->n;i++)
-            if(max<v[i]){
-                max=v[i];
-                poz=i;
-            }
-            cout<<"Maximul este "<<max<<" pe pozitia "<<poz<<endl;
-    }
-void vec::sortare(){                                //sortarea crescatoare a vectorului
-    int aux;
-    for(int i=0; i<this->n-1; i++)
-        for(int j=0;j<this->n-i-1;j++)
-        {
-            if(this->v[j]>this->v[j+1])
-            {
-                aux=this->v[j];
-                this->v[j]=this->v[j+1];
-                this->v[j+1]=aux;
-
-            }
+int Faina::calcul_cost(){
+    f>>nr_kg;
+    f>>calitate;
+    if(calitate == 1)
+        {pret = 5;
+        stoc =20;}
+    else if(calitate == 2)
+        {pret = 3;
+        stoc = 30;}
+    else if(calitate == 3)
+        {pret = 2;
+        stoc = 40;}
+    else if(calitate ==NULL)
+        calitate = 1;
+    if(nr_kg > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de kilograme de faina, calitatea "<<calitate<<". Doriti sa cumparati "<< stoc << " de kilograme?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<nr_kg; i++)
+            cost = pret * nr_kg;
+    cout<<"Costul pentru faina este: "<<cost<<endl;
+    return cost;
+}
+int Varza::calcul_cost(){
+    f>>nr_bucati;
+    pret = 5;
+    stoc = 50;
+    if(nr_bucati <= stoc)
+    for(int i=1; i<=nr_bucati; i++)
+            cost = nr_bucati * pret;
+    if(nr_bucati > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de bucati de varza. Doriti sa cumparati "<< stoc << " de bucati?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;
         }
+    else
+        for(int i=0; i<nr_bucati; i++)
+            cost = pret *nr_bucati;
 
+    cout<<"Costul pentru toate bucatile de varza este: "<<cost<<endl;
+    return cost;
 }
-int vec::operator * (vec const &ob1){               //suprainarcarea op "*" = produs scalar
-    int produs=0;
-    if(this->n==ob1.n)                       //verificam daca vectorii au aceeasi lungime pt a putea fi inmultiti
-    {for(int i=0;i<this->n;i++)
-        produs = produs+(ob1.v[i])*this->v[i];}
+int Vin_varsat::calcul_cost(){
+    f>>tip;
+    f>>litri;
+    if(tip == "rosu_sec")
+        {pret = 10;
+        stoc = 50;}
+    else if(tip == "alb_sec")
+       {pret = 12;
+       stoc = 100;}
+    else if(tip.empty())
+        tip = "rosu_sec";
+   if(litri > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de litri de vin "<<tip<<". Doriti sa cumparati "<< stoc << " de bucati?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<litri; i++)
+        cost = pret * litri;
+    cout<<cost;
+    return cost;
+}
+int vin_soi::calcul_cost(){
+    f>>buc;
+    f>>soi;
+    f>>an_culegere;
+    f>>tara_origine;
+
+    if(soi == "Cabernet Savignon")
+       {
+           stoc = 100;
+           if(an_culegere<1950)
+            {
+                if(tara_origine == "Franta")
+                    pret = 100;
+                else if(tara_origine == "Argentina")
+                    pret = 120;
+                else if(tara_origine == "Chile")
+                    pret == 150;
+                else if(tara_origine == "Australia")
+                    pret == 200;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+            }
+
+        else if(an_culegere>=1950)
+                {if(tara_origine == "Franta")
+                    pret = 50;
+                else if(tara_origine == "Argentina")
+                    pret = 60;
+                else if(tara_origine == "Chile")
+                    pret == 70;
+                else if(tara_origine == "Australia")
+                    pret == 80;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+                    }
+       }
+    else if(soi == "Merlot")
+        {   stoc = 70;
+            if(an_culegere<1950)
+            {
+                if(tara_origine == "Franta")
+                    pret = 200;
+                else if(tara_origine == "Argentina")
+                    pret = 220;
+                else if(tara_origine == "Chile")
+                    pret == 250;
+                else if(tara_origine == "Australia")
+                    pret == 300;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+            }
+        else if(an_culegere>=1950)
+                {if(tara_origine == "Franta")
+                    pret = 150;
+                else if(tara_origine == "Argentina")
+                    pret = 160;
+                else if(tara_origine == "Chile")
+                    pret == 170;
+                else if(tara_origine == "Australia")
+                    pret == 180;}
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+                    }
+    else if(soi == "Savignon Blanc")
+       {    stoc = 40;
+           if(an_culegere<1950)
+            {
+                if(tara_origine == "Franta")
+                    pret = 300;
+                else if(tara_origine == "Argentina")
+                    pret = 320;
+                else if(tara_origine == "Chile")
+                    pret == 350;
+                else if(tara_origine == "Australia")
+                    pret == 300;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+            }
+        else if(an_culegere>=1950)
+                {if(tara_origine == "Franta")
+                    pret = 250;
+                else if(tara_origine == "Argentina")
+                    pret = 260;
+                else if(tara_origine == "Chile")
+                    pret == 270;
+                else if(tara_origine == "Australia")
+                    pret == 280;}
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+                    }
+    else if(soi == "Chardonnay")
+       {    stoc = 30;
+           if(an_culegere<1950)
+            {
+                if(tara_origine == "Franta")
+                    pret = 400;
+                else if(tara_origine == "Argentina")
+                    pret = 420;
+                else if(tara_origine == "Chile")
+                    pret == 450;
+                else if(tara_origine == "Australia")
+                    pret == 500;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;
+            }
+        else if(an_culegere>=1950)
+                {if(tara_origine == "Franta")
+                    pret = 350;
+                else if(tara_origine == "Argentina")
+                    pret = 360;
+                else if(tara_origine == "Chile")
+                    pret == 370;
+                else if(tara_origine == "Australia")
+                    pret == 380;
+                else
+                    cout<<"Produsul nu este in stoc."<<endl;}}
+
         else
-            cout<<"Vectorii nu se pot inmulti."<<endl;
-    return produs;
-}
-vec* vec::cma(){                                    //citirea, memorarea si afisarea a n obiecte
-    cout<<"Introduceti nr de obiecte pentru citit: ";
-    int n;
-    vec *obiecte=new vec[n];                            //vector de obiecte
-    cin>>n;
-    for(int i=0;i<n;i++)
-          {cin>>obiecte[i];
-            cout<<obiecte[i];}
-    return obiecte;
+            {soi = "Cabernet Savignon";
+               an_culegere = 1960;
+               tara_origine = "Franta";
+               pret = 50;}
+
+    if(buc > stoc)
+        {cout<<"In stoc exista doar "<<stoc<<" de bucati de vin. Doriti sa cumparati "<< stoc << " de bucati?"<<endl;
+            cin>>raspuns;
+        if(raspuns == 'd')
+            cost = pret * stoc;
+        else if (raspuns == 'n')
+            cost = 0;}
+    else
+        for(int i=0; i<buc; i++)
+        cost = pret * buc;
+        cout<<"Costul pentru vinul de soi este "<<cost<<endl;
+        return cost;
 }
 
 int main()
 {
-    /*vec ob1(2,3);
-    cin>>ob1;
-    cout<<ob1;
-    ob1.suma();
-    ob1.maxim();
-    cout<<"Dupa ordonare:"<<endl;
-    ob1.sortare();
-    cout<<ob1;
-    vec ob2=ob1;
-    cout<<ob2;
-    if(ob1*ob2 != 0)    //daca e 0 inseamna ca vectorii nu au acelasi nr de elemente si va afisa mesaj
-        cout<<"Produsul vectorial este: "<<ob1*ob2;
-    return 0;*/
-    vec * obiecte;
-    obiecte = vec::cma();
+    int buget_magazin = 0;
+    int i, n=4, s=0,total = 0,vec[100]= {0};
+    string a[100];
+    Produse p;
+    Varza varza;
+    Faina faina;
+    Bere bere;
+    Cartofi cartofi;
+    Jucarii jucarii;
+    Vin_varsat varsat;
+    vin_soi soi;
+    for(i=0; i<n; i++)
+    {f>>a[i];
+    //cout<<a[i]<<" "<<endl;
+    if(a[i] == "varza")
+        vec[i] = varza.calcul_cost();
+    if(a[i] == "faina")
+        vec[i] = faina.calcul_cost();
+    if(a[i] == "vin_soi")
+        vec[i] = soi.calcul_cost();
+    if(a[i]=="bere")
+        vec[i] = bere.calcul_cost();
+    if(a[i]=="vin_varsat")
+        vec[i] = varsat.calcul_cost();
+    if(a[i]=="jucarie")
+        vec[i] = jucarii.calcul_cost();
+    if(a[i]=="cartofi")
+        vec[i] = cartofi.calcul_cost();
+    total = total + vec[i];
+        }
+        cout<<"Totalul pentru cumparaturile efectuate este: " <<total << "lei"<<endl;
+        cout<<"Doriti sa cumparati?"<<endl;
+        char rasp;
+        cin>>rasp;
+        if(rasp == 'd')
+            buget_magazin = buget_magazin + total;
+        cout<<"Bugetul magazinului este: "<<buget_magazin;
+        out.open("inventar",std::ios::app);
+        out<<buget_magazin;
+        out<<"\n";
+
+
+    return 0;
 }
